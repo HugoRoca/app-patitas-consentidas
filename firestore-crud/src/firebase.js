be3c789779dda69e0ac9083afa76e7e2024a50a8
Hugo Roca <hugo.rock20@hotmail.com>
2020-08-19 21:49:24 -0500

    create project and more

---
+++ firestore-crud/src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+  
+};
+// Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig);
+
+export const db = fb.firestore()
+
