7756ab8ae759ece614e8e11e1258d4de44bd0d09
Hugo Roca <hugo.rock20@hotmail.com>
2020-08-19 23:30:46 -0500

    projecy example finish

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Links from "./components/Links";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="container p-4">
+      <Links />
+      <ToastContainer/>
+    </div>
   );
 }
 
